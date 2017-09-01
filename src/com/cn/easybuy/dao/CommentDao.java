b414b9e8df04fda247009a586e0b364fe79e8bb5
garfunke1 <garfunkel_@outlook.com>
2017-09-01 21:41:25 +0800

    upload

--- src/com/cn/easybuy/dao/CommentDao.java
+++ src/com/cn/easybuy/dao/CommentDao.java
@@ -7,8 +7,12 @@ import com.cn.easybuy.util.Pages;
 
 public interface CommentDao {
 	public List<Comment> getCommentLimitPage(Pages page);
+
 	public Comment queryCommentById(int id);
+
 	public boolean updateReplyById(int id, String replyContent);
+
 	public boolean addContentById(String name, String Content);
+
 	public boolean deleteCommentById(int id);
 }
